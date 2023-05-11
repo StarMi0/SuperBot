3cda61a55cef91ed361bb25bf84eda642be8499f
StarMi0 <starscream2705@gmail.com>
2023-05-11 03:39:02 +0300

    Добавлен запуск ботак ВК

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from aiogram import executor
-from create_bot import dp
+from create_bot import dp, vk_session, vk, upload
 from Telegramm import bot
-import sqlite3
+from vk_api.bot_longpoll import VkBotLongPoll
+from functions.functions import key_dict
+from VK.vk import run_vk_bot
 
 
 async def on_startup(_):
@@ -12,3 +14,6 @@ bot.register_handler_client(dp)
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=False, on_startup=on_startup)
+    longpoll = VkBotLongPoll(vk_session, key_dict['VK_GROUP_ID'])
+    print("VK bot is runned...")
+    run_vk_bot(longpoll, vk, upload)
