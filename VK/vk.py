33ed95951160b11d677db5556a57a92dc74fdcf1
StarMi0 <starscream2705@gmail.com>
2023-05-11 03:41:25 +0300

    доработана выдача купонов

--- VK/vk.py
+++ VK/vk.py
@@ -1,41 +1,68 @@
+import random
+import time
+
+import requests
 import vk_api
-from vk_api.bot_longpoll import VkBotLongPoll
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from functions.functions import get_discount_code, key_dict
 
-GROUP_ID = 12345  # Replace with your group ID
+GROUP_ID = key_dict['VK_GROUP_ID']  # Replace with your group ID
 
-# Connect to VK API
-vk_session = vk_api.VkApi(token=key_dict['VKID'])
-longpoll = VkBotLongPoll(vk_session, GROUP_ID)
-vk = vk_session.get_api()
 
-for event in longpoll.listen():
-    if event.type == VkBotEventType.MESSAGE_NEW and event.object.text.lower() == 'хочу купон':
-        # Check if user is subscribed to the group
-        is_subscribed = vk.groups.isMember(
-            group_id=GROUP_ID,
-            user_id=event.object.from_id
-        )
+def run_vk_bot(longpoll, vk, upload):
+    try:
+        for event in longpoll.listen():
 
-        if is_subscribed:
-            # Get discount code link
-            link = get_discount_code('vk', event.object.from_id)
-            if link:
-                # Open image and send it to the user
-                image = open(link)
-                vk.messages.send(
-                    user_id=event.object.from_id,
-                    attachment=image,
-                )
-            else:
-                # Send message about existing active coupon
-                vk.messages.send(
-                    user_id=event.object.from_id,
-                    message="You already have an active coupon."
-                )
-        else:
-            # Send message about subscription
-            vk.messages.send(
-                user_id=event.object.from_id,
-                message="Subscribe to our channel and try again."
-            )
+            if event.type == VkBotEventType.MESSAGE_NEW and \
+                    event.object['message']['from_id'] and \
+                    event.object['message']['text']:
+
+                if event.object['message']['text'] == 'Хочу купон':
+                    # Check if user is subscribed to the group
+
+                    is_subscribed = vk.groups.isMember(
+                        group_id=GROUP_ID,
+                        user_id=event.object['message']['from_id'],
+                    )
+
+                    if is_subscribed:
+                        # Get discount code link
+                        link = get_discount_code('vk', event.object['message']['from_id'])
+                        if link:
+                            # Open image and send it to the user
+
+                            image = open(link, 'rb')
+                            upload_image = upload.photo_messages(photos=image,
+                                                                 peer_id=event.object['message']['from_id'])[0]
+                            pic = f"photo{upload_image['owner_id']}_{upload_image['id']}"
+                            vk.messages.send(
+                                user_id=event.object['message']['from_id'],
+                                attachment=pic,
+                                random_id=random.randint(0, 100)
+                            )
+                        else:
+                            # Send message about existing active coupon
+                            vk.messages.send(
+                                user_id=event.object['message']['from_id'],
+                                message="У Вас уже есть активный купон.",
+                                random_id=random.randint(0, 100)
+                            )
+                    else:
+                        # Send message about subscription
+                        vk.messages.send(
+                            user_id=event.object['message']['from_id'],
+                            message=f'Подпишитесь на наш канал: {key_dict["url_vk"]}\n,'
+                                    f'чтобы получить купон и повторите попытку.',
+                            random_id=random.randint(0, 100)
+                        )
+                else:
+                    text = f"Не понял, что вы хотели сказать: {event.object['message']['text']}"
+
+                    vk.messages.send(
+                        user_id=event.object['message']['from_id'],
+                        message=text,
+                        random_id=random.randint(0, 100)
+                    )
+    except requests.exceptions.ReadTimeout:
+        print("\n Переподключение к серверам ВК \n")
+        time.sleep(3)
