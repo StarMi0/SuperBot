bfe7143d72d539e123b11036e4df72e91ad86359
StarMi0 <starscream2705@gmail.com>
2023-05-17 15:06:08 +0300

    сделан асинхронный запуск ботов

--- create_bot_tg.py
+++ create_bot_tg.py
@@ -1,12 +1,10 @@
 import asyncio
 
 from aiogram import Bot, Dispatcher
-from aiogram.types import message
-
-from create_bot_vk import run_vk
-
 
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+from create_bot_vk import run_vk
 from functions.functions import key_dict
 
 # Connect to Telegramm API
@@ -14,11 +12,8 @@ bot = Bot(token=key_dict['TGID'])
 dp = Dispatcher(bot, storage=MemoryStorage())
 
 
-loop = asyncio.get_event_loop()
-
-
 async def on_startup(_):
-    # asyncio.create_task(run_vk())
+    asyncio.create_task(run_vk())
     print('Telegram bot is online')
 
 
--- main.py
+++ main.py
@@ -8,15 +8,15 @@ import asyncio
 bot.register_handler_client(dp)
 
 
-ioloop = asyncio.get_event_loop()
-tasks = [
-    ioloop.create_task(run_vk()),
-    ioloop.create_task(executor.start_polling(dp, skip_updates=False, on_startup=on_startup))
-]
+# ioloop = asyncio.get_event_loop()
+# tasks = [
+#     ioloop.create_task(run_vk()),
+#     ioloop.create_task(executor.start_polling(dp, skip_updates=False, on_startup=on_startup))
+# ]
 
 
 if __name__ == "__main__":
-    # executor.start_polling(dp, skip_updates=False, on_startup=on_startup)
-    ioloop.run_until_complete(asyncio.wait(tasks))
-    ioloop.close()
+    executor.start_polling(dp, skip_updates=False, on_startup=on_startup)
+    # ioloop.run_until_complete(asyncio.wait(tasks))
+    # ioloop.close()
 
