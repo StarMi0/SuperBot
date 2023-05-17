a8198e9a26a6b73e380eedf4a26a7cb4430b310b
StarMi0 <starscream2705@gmail.com>
2023-05-17 14:05:35 +0300

    удалены функции администратора, добавлена кнопка под сообщением

--- Telegramm/bot.py
+++ Telegramm/bot.py
@@ -2,7 +2,6 @@ from aiogram import types, Dispatcher
 from functions.functions import get_discount_code, key_dict
 from create_bot_tg import dp, bot
 from .keyboard import kb_client
-import re
 
 
 async def check_client(message: types.Message):
@@ -23,18 +22,43 @@ async def check_client(message: types.Message):
         await message.delete()
 
 
-async def command_start(message: types.Message):
+async def callback_client(callback_query: types.CallbackQuery):
+    user_id = callback_query.from_user.id  # ID пользователя который запустил хэндлер
 
+    if False:
+        await bot.send_message(callback_query.from_user.id, f'Подпишитесь на наш канал: {key_dict["url_tg"]}\n,'
+                                                            f'чтобы получить купон и повторите попытку.')
+    else:
+        # Генерация и присылка купона:
+        cupon_path = get_discount_code('tg', user_id)
+        if cupon_path:
+            photo = open(cupon_path, 'rb')
+            await bot.send_photo(callback_query.from_user.id, photo=photo)
+        else:
+            await bot.send_message(callback_query.from_user.id, "У Вас уже есть активный купон.")
+
+
+async def command_start(message: types.Message):
+    user_name = message.from_user.full_name
+    print(user_name)
     try:
-        await bot.send_message(message.from_user.id, f"Если вы еще не получали скидочный купон, "
-                                                     f"нажмите 'Хочу купон' или введите сообщение 'Хочу купон'",
+        await bot.send_message(message.from_user.id, f"Привет, {user_name}!\n"
+                                                     f"Я бот Наша Обувь.\n"
+                                                     f"Чтобы получить купон 500 руб, нажми\n"
+                                                     f"КУПОН",
                                reply_markup=kb_client)
         await message.delete()
-    except:
-        await message.reply(f'Ошибка соединения...')
+    except ValueError as err:
+        dict_response = {
+            'error': err.args[0],
+        }
+        await message.reply(f'Ошибка соединения...\n{dict_response}')
 
 
 def register_handler_client(dp: Dispatcher):
-
-    dp.register_message_handler(check_client, text='Хочу купон')
+    dp.register_callback_query_handler(
+        callback_client,
+        lambda callback_query: True
+    )
+    dp.register_message_handler(check_client, text='КУПОН')
     dp.register_message_handler(command_start, state='*', content_types=['text'])
--- Telegramm/keyboard.py
+++ Telegramm/keyboard.py
@@ -1,19 +1,7 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, ReplyKeyboardRemove
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
-coupon = KeyboardButton('Хочу купон')
+coupon = InlineKeyboardButton('👉КУПОН👈', callback_data="button")
 
-vk_button = KeyboardButton('vk')
-tg_button = KeyboardButton('tg')
-
-cansel_b = KeyboardButton('cansel')
-
-add_user = KeyboardButton('add user')
-get_dis = KeyboardButton('get')
-
-kb_client = ReplyKeyboardMarkup(resize_keyboard=True)
-kb_admin = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-kb_admin1 = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+kb_client = InlineKeyboardMarkup(resize_keyboard=True)
 
 kb_client.add(coupon)
-kb_admin.add(vk_button).add(tg_button).add(cansel_b)
-kb_admin1.add(add_user).add(get_dis)
