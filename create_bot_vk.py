89b8463fcf2e4c783cf962a40daa292bb188dd90
StarMi0 <starscream2705@gmail.com>
2023-05-18 09:37:43 +0300

    сделан асинхронный запуск ботов

--- create_bot_vk.py
+++ create_bot_vk.py
@@ -1,3 +1,4 @@
+import requests
 import vk_api
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from functions.functions import key_dict
@@ -5,18 +6,21 @@ from VK.vk import run_vk_bot
 
 
 # Connect to VK API
-vk_session = vk_api.VkApi(token=key_dict['VKID'])
-vk = vk_session.get_api()
-upload = vk_api.VkUpload(vk)
-longpoll = VkBotLongPoll(vk_session, key_dict['VK_GROUP_ID'])
 
 
 async def run_vk():
-    run_vk_bot(longpoll, vk, upload)
-    print("VK bot is online!!!")
+    while True:
+        session = requests.Session()
+        vk_session = vk_api.VkApi(token=key_dict['VKID'])
+        vk = vk_session.get_api()
+        upload = vk_api.VkUpload(vk)
+        longpoll = VkBotLongPoll(vk_session, key_dict['VK_GROUP_ID'])
+        try:
+            run_vk_bot(longpoll, vk, upload)
+            print("VK bot is online!!!")
+        except Exception:
+            pass
 
 
 if __name__ == "__main__":
-    run_vk_bot(longpoll, vk, upload)
-
-
+    run_vk()
